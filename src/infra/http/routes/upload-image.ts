33f713dcbff673dd49ba3c7b9ff9c5e851c71527
Gabriel Desidério <gabrieldesd7@gmail.com>
2025-01-15 20:38:01 -0300

    upload image to route

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -9,10 +9,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
     {
       schema: {
         summary: 'Upload an image',
-        body: z.object({
-          name: z.string(),
-          password: z.string().optional(),
-        }),
+        consumes: ['multipart/form-data'],
         response: {
           201: z.object({ uploadId: z.string() }),
           409: z
@@ -24,11 +21,14 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       },
     },
     async (request, reply) => {
-      await db.insert(schema.uploads).values({
-        name: 'test.jpg',
-        remoteKey: 'teste.jpg',
-        remoteUrl: 'http://upload.com/teste.jpg',
+      const uploadedFile = await request.file({
+        limits: {
+          fileSize: 1024 * 1024 * 2, // 2 mb
+        },
       })
+
+      console.log(uploadedFile)
+
       return reply.status(201).send({ uploadId: 'test' })
     }
   )
