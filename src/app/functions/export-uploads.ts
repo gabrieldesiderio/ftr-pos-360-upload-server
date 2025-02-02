2ed26349ff8c96793f25465ae43c240d27bb84c0
Gabriel Desidério <gabrieldesd7@gmail.com>
2025-02-02 13:18:12 -0300

    export uploads function and route

---
+++ src/app/functions/export-uploads.ts
@@ -0,0 +1,80 @@
+import { PassThrough, Transform } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
+import { db, pg } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { uploadFileToStorage } from '@/infra/storage/upload-file-to-storage'
+import { type Either, makeRight } from '@/shared/either'
+import { stringify } from 'csv-stringify'
+import { ilike } from 'drizzle-orm'
+import { z } from 'zod'
+
+const exportUploadsInput = z.object({
+  searchQuery: z.string().optional(),
+})
+
+type ExportUploadsInput = z.input<typeof exportUploadsInput>
+
+type ExportUploadsOutput = {
+  reportUrl: string
+}
+
+export async function exportUploads(
+  input: ExportUploadsInput
+): Promise<Either<never, ExportUploadsOutput>> {
+  const { searchQuery } = exportUploadsInput.parse(input)
+
+  const { sql, params } = db
+    .select({
+      id: schema.uploads.id,
+      name: schema.uploads.name,
+      remoteUrl: schema.uploads.remoteUrl,
+      createdAt: schema.uploads.createdAt,
+    })
+    .from(schema.uploads)
+    .where(
+      searchQuery ? ilike(schema.uploads.name, `%${searchQuery}%`) : undefined
+    )
+    .toSQL()
+
+  const cursor = pg.unsafe(sql, params as string[]).cursor(2)
+
+  const csv = stringify({
+    delimiter: ',',
+    header: true,
+    columns: [
+      { key: 'id', header: 'ID' },
+      { key: 'name', header: 'Name' },
+      { key: 'remote_url', header: 'URL' },
+      { key: 'created_at', header: 'Uploaded At' },
+    ],
+  })
+
+  const ulpoadToStorageStream = new PassThrough()
+
+  const convertToCSVPipeline = await pipeline(
+    cursor,
+    new Transform({
+      objectMode: true,
+      transform(chunks: unknown[], enconding, callback) {
+        for (const chunk of chunks) {
+          this.push(chunk as Buffer)
+        }
+
+        callback()
+      },
+    }),
+    csv,
+    ulpoadToStorageStream
+  )
+
+  const ulpoadToStorage = uploadFileToStorage({
+    contentType: 'text/csv',
+    folder: 'downloads',
+    fileName: `${new Date().toISOString()}-uploads.csv`,
+    contentStream: ulpoadToStorageStream,
+  })
+
+  const [{ url }] = await Promise.all([ulpoadToStorage, convertToCSVPipeline])
+
+  return makeRight({ reportUrl: url })
+}
--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -39,8 +39,6 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       }
 
       if (isRight(result)) {
-        console.log(unwrapEither(result))
-
         return reply.status(201).send()
       }
 
